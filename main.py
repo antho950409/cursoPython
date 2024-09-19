bacc2b36c34607ad3913412d89a65683194d0473
antho950409 <a20244911@pucp.edu.pe>
2024-09-18 22:00:18 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+print("Hola mundo")
+a = 10
+b = 3
+
+# Operaciones básicas
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+modulo = a % b  # Resto de la división
+
+print("Suma:", suma)
+print("Resta:", resta)
+print("Multiplicación:", multiplicacion)
+print("División:", division)
+print("Módulo:", modulo)
